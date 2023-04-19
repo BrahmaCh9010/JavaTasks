11859cf5a189b560b3354b6b1e943f9837a22dce
Brahma <bathulabrahmaiah@gmail.com>
2023-04-19 15:56:14 +0530

    Calculations with User Input Values

---
+++ src/com/java_training_tasks/Calculator_App.java
@@ -0,0 +1,105 @@
+package com.java_training_tasks;
+
+import java.util.Scanner;
+
+public class Calculator_App {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		double num1;
+		double num2;
+		while(true) {
+			System.out.println("1. Addition");
+			System.out.println("2. Subtraction");
+			System.out.println("3. Multiplication");
+			System.out.println("4. Division");
+			System.out.println("5. Modulus");
+			System.out.println("6. Power");
+			System.out.println("7. Square root");
+			System.out.println("8. Exit");
+			
+			int operations = sc.nextInt();
+			switch (operations) {
+			
+			case 1: {
+				System.out.println("Enter First Number : ");
+				num1 = sc.nextDouble();
+				System.out.println("Enter Second Number : ");
+				num2 = sc.nextDouble();
+				double sum = num1 + num2;
+				System.out.println("Result : "+sum);
+				break;
+			}
+			case 2: {
+				System.out.println("Enter First Number : ");
+				num1 = sc.nextDouble();
+				System.out.println("Enter Second Number : ");
+				num2 = sc.nextDouble();
+				double sub = num1 - num2;
+				System.out.println("Result : "+sub);
+				break;
+			}
+			case 3: {
+				System.out.println("Enter First Number : ");
+				num1 = sc.nextDouble();
+				System.out.println("Enter Second Number : ");
+				num2 = sc.nextDouble();
+				double mul = num1 * num2;
+				System.out.println("Result : "+mul);
+				break;
+			}
+			case 4: {
+				System.out.println("Enter First Number : ");
+				num1 = sc.nextDouble();
+				System.out.println("Enter Second Number : ");
+				num2 = sc.nextDouble();
+				if(num2 == 0) {
+					System.out.println("Not divsible by 0");
+				} else {
+					double div = num1 / num2;
+					System.out.println("Result : "+div);
+				}
+				break;
+			}
+			case 5: {
+				System.out.println("Enter First Number : ");
+				num1 = sc.nextDouble();
+				System.out.println("Enter Second Number : ");
+				num2 = sc.nextDouble();
+				double mod = num1 % num2;
+				System.out.println("Result : "+mod);
+				break;
+			}
+			case 6: {
+				System.out.println("Enter First Number : ");
+				num1 = sc.nextDouble();
+				System.out.println("Enter Second Number : ");
+				num2 = sc.nextDouble();
+				double pow = Math.pow(num1, num2); 
+				System.out.println("Result : "+pow);
+				break;
+			}
+			case 7: {
+				System.out.println("Enter First Number : ");
+				num1 = sc.nextDouble();
+				if(num1 < 0) {
+					System.out.println("Square with negative numbers not posible");
+				} else {
+					double sqrt = Math.sqrt(num1);
+					System.out.println("Result : "+sqrt);
+				}
+				break;
+			}
+			case 8: {
+				System.out.println("Exit Calculation....");
+				return;
+			}
+			default:
+				System.out.println("Invalid operation:");
+				break;
+			}
+		}
+	}
+
+}
